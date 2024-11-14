49f4ebf4c76081af531ce5089b73ffd787f3c3fd
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2024-11-14 03:50:07 +0300

    a

--- 2/A.py
+++ 2/A.py
@@ -1,4 +1,20 @@
-stack=[]
-cmd=input()
+stack = []
+cmd = input()
 
 while cmd != 'exit':
+    if 'push' in cmd:
+        pussh, n = cmd.split()
+        stack.append(n)
+        print('ok')
+    elif 'pop' in cmd:
+        print(stack.pop())
+    elif 'back' in cmd:
+        print(stack[-1])
+    elif 'size' in cmd:
+        print(len(stack))
+    elif 'clear' in cmd:
+        stack.clear()
+        print('ok')
+    cmd = input()
+print('bye')
+
