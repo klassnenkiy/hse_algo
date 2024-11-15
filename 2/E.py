7848ae3a47b5fe71102c00008ebd876577264977
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2024-11-15 03:15:56 +0300

    e

---
+++ 2/E.py
@@ -0,0 +1,20 @@
+l1 = list(map(int, input().split()))
+l2 = list(map(int, input().split()))
+
+card = 0
+while l1 and l2:
+    card += 1
+    first = l1.pop(0)
+    second = l2.pop(0)
+    if (first>second) and (str(first)+str(second) != '90') or (str(first)+str(second) == '09'):
+        l1 += [first, second]
+    else:
+        l2 += [first, second]
+    if card == 10**6:
+        print('botva')
+        break
+else:
+    if l2:
+        print('second',card)
+    else:
+        print('first',card)
